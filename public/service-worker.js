973e3e4e547af8161c531d9984004065a36d2566
elijahb22 <elibusick11@gmail.com>
2022-03-19 17:44:13 -0400

    added event listener to install and activate

--- public/service-worker.js
+++ public/service-worker.js
@@ -19,3 +19,36 @@ const FILES_TO_CACHE = [
     "/icons/icon-72x72.png"
 ];
 
+self.addEventListener('install', function (e) {
+    e.waitUnntil(
+      caches.open(CACHE_NAME).then(function (cache) {
+        console.log('installing cache : ' + CACHE_NAME)
+        // add every file in the FILES_TO_CACHE array to the cache
+        return cache.addAll(FILES_TO_CACHE)
+      })
+    )
+})
+
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        // .keys() returns an array of all cache names, which we're calling keyList. keyList is a parameter that contains all cache names under <username>.github.io
+      caches.keys().then(function (keyList) {
+        let cacheKeeplist = keyList.filter(function (key) {
+          return key.indexOf(APP_PREFIX);
+        });
+
+    cacheKeeplist.push(CACHE_NAME);
+
+    //Remember that we set up CACHE_NAME as a global constant to help keep track of which cache to use. Finish the routine with the return statement shown in the following sample:
+
+    return Promise.all(keyList.map(function (key, i) {
+            if (cacheKeeplist.indexOf(key) === -1) {
+                console.log('deleting cache : ' + keyList[i] );
+                return caches.delete(keyList[i]);
+            }
+        })
+    );
+    })
+   );
+});
+
